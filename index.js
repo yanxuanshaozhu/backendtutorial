1e35605cc0224590d58ea04f041abe4c8a67737f
Ian <iansmith@brandeis.edu>
2022-02-05 11:29:43 -0500

    add ship code to index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-const router = express.Router();
 const PORT = 3000;
+
+let newShip = require("./routes/newShip");
+
+// on every request, parse the request body using this library.
+app.use(bodyParser.json());
+// on a request to /, use the newShip code.
+// note that the entire URL including the one declared in the file must match.
+app.use("/",newShip);
