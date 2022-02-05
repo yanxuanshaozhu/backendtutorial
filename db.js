1c6da76ef93de22dd58e9a90f7916b9489dfc2e1
Ian <iansmith@brandeis.edu>
2022-02-05 11:29:05 -0500

    prepare DB file

--- db.js
+++ db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 /**
  * URL to the MongoDB instance.
  */
-const MONGODB_URL = "<your MongoDB URL with <password> replaced with your password>";
+const MONGODB_URL = "REPLACE THIS WITH YOUR DB URL";
 
 // Tell Mongoose to connect to the MongoDB instance using the provided configuration.
-mongoose.connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(MONGODB_URL);
 
 /**
  * Mongoose connection to the MongoDB instance.
@@ -17,10 +14,10 @@ let db = mongoose.connection;
 
 // Listen for errors and print them to the console.
 db.on("error", function (err) {
-    console.error("Mongoose Error: ", err);
+  console.error("Mongoose Error: ", err);
 });
 
 // When the MongoDB connection is made, print to the console.
 db.once("open", function () {
-    console.info("Mongoose connection successful.");
+  console.info("Mongoose connection successful.");
 });
